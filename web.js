f431b71e0cd66f2bd993b519cb7ff637ba93bc66
stephendself <ubuntu@ip-172-31-42-234.us-west-2.compute.internal>
2013-07-14 07:23:40 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require ('fs');
+
+fs.readfile(index.html);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('fs.readfile(index.html)');
 });
 
 var port = process.env.PORT || 5000;
